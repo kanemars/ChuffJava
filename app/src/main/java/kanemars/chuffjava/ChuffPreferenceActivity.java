54e4c4713677b4140699f8a78ffe93fad53dd745
Kane Marshall <kanemars@hotmail.com>
2017-01-27 16:04:34 +0000

    Show notification switch which Toasts when it has been turned on

--- app/src/main/java/kanemars/chuffjava/ChuffPreferenceActivity.java
+++ app/src/main/java/kanemars/chuffjava/ChuffPreferenceActivity.java
@@ -1,11 +1,13 @@
 package kanemars.chuffjava;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
+import android.preference.*;
+import android.text.format.DateFormat;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class ChuffPreferenceActivity extends PreferenceActivity {
 
@@ -32,7 +34,21 @@ public class ChuffPreferenceActivity extends PreferenceActivity {
                 public boolean onPreferenceChange(Preference arg0, Object isNotificationOnObj) {
                     boolean isNotificationOn = (Boolean) isNotificationOnObj;
                     if (isNotificationOn) {
-                        Toast.makeText(getActivity(), "Turned on!", Toast.LENGTH_SHORT).show();
+
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        String strSource = sharedPreferences.getString("edit_text_source", "Taplow - TAP");
+                        String strDestination = sharedPreferences.getString("edit_text_destination", "Reading - RDG");
+
+
+
+                        long strNotificationTime = sharedPreferences.getLong( "notification_time", 1232);
+                        Calendar timeToNotify = Calendar.getInstance();
+                        timeToNotify.setTimeInMillis(strNotificationTime);
+
+                        String time = DateFormat.getTimeFormat(getContext()).format(new Date(timeToNotify.getTimeInMillis()));
+
+                        Toast.makeText(getActivity(), String.format("Notifications set up for %s to %s at %s",
+                                strSource, strDestination, time), Toast.LENGTH_SHORT).show();
                     }
                     return true;
                 }
