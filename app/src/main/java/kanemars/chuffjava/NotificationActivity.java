4b698120c97f5a63cf105b780f546aa974d74dd8
Kane Marshall <kanemars@hotmail.com>
2017-03-16 11:01:25 +0000

    A blank activity (with notification for proof) is started when android is rebooted!

--- app/src/main/java/kanemars/chuffjava/NotificationActivity.java
+++ app/src/main/java/kanemars/chuffjava/NotificationActivity.java
@@ -1,7 +1,13 @@
 package kanemars.chuffjava;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.NotificationCompat;
 
 public class NotificationActivity extends Activity
 {
@@ -9,5 +15,23 @@ public class NotificationActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        showNotification(this);
+    }
+
+    private void showNotification(Context context) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_stat_train)
+                        .setContentTitle("Chuff Me Activity")
+                        .setContentText("Started from NotificationActivity");
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
     }
 }
--- app/src/main/java/kanemars/chuffjava/StartMainActivityAtBootReceiver.java
+++ app/src/main/java/kanemars/chuffjava/StartMainActivityAtBootReceiver.java
@@ -6,29 +6,19 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.NotificationCompat;
 
 public class StartMainActivityAtBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // This gets called when Android starts up
-        showNotification(context);
+        startActivity(context);
     }
 
-    private void showNotification(Context context) {
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class), 0);
-
-        NotificationCompat.Builder mBuilder =
-                (NotificationCompat.Builder) new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_stat_train)
-                        .setContentTitle("Chuff Me")
-                        .setContentText("The App Chuff Me started this notification");
-        mBuilder.setContentIntent(contentIntent);
-        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
-        mBuilder.setAutoCancel(true);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(1, mBuilder.build());
+    public void startActivity (Context context){
+        Intent i = new Intent();
+        i.setClassName("kanemars.chuffjava", "kanemars.chuffjava.NotificationActivity");
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
+
 }
