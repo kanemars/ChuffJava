8e88fe7068461c1a50dcd93d3cdc27182c2a42bf
Kane Marshall <kanemars@hotmail.com>
2017-03-16 12:05:15 +0000

    Removed the test NotificationActivity and now starting the MainActivity. It just doesn't start notifications if they're turned on!

--- app/src/main/java/kanemars/chuffjava/StartMainActivityAtBootReceiver.java
+++ app/src/main/java/kanemars/chuffjava/StartMainActivityAtBootReceiver.java
@@ -1,8 +1,5 @@
 package kanemars.chuffjava;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,12 +8,12 @@ public class StartMainActivityAtBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // This gets called when Android starts up
-        startActivity(context);
+        startMainActivity(context);
     }
 
-    public void startActivity (Context context){
+    private void startMainActivity(Context context){
         Intent i = new Intent();
-        i.setClassName("kanemars.chuffjava", "kanemars.chuffjava.NotificationActivity");
+        i.setClassName("kanemars.chuffjava", "kanemars.chuffjava.MainActivity");
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
     }
